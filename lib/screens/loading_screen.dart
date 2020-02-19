87ee350b71dd0250001c325b02a3f494c7eef940
ricky <rikanthanricky@gmail.com>
2020-02-19 07:32:27 +0530

    api key

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'location.dart';
+
+
+const Apikey='067ad514cf62f9ba62b5ea5d5ffa57d1';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,6 +18,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState(){
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async
@@ -27,11 +33,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getData() async
   {
-    http.Response response=await http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22');
+    http.Response response=await http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=$Apikey');
 
     if(response.statusCode==200)
       {
         String data=response.body;
+        var decodes=jsonDecode(data);
+
+        double temperature=decodes['main']['temp'];
+        int condition=decodes['weather'][0]['id'];
+        String city=decodes['name'];
+        print(temperature);
+        print(condition);
+       print(city);
+
       }
     else{
       print(response.statusCode);
